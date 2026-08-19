c9a5f85c6a76f663386182a1625e8dea17af80fe
lasautt-ops <lasautt@gmail.com>
2026-08-19 12:54:39 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const prisma = new PrismaClient();
