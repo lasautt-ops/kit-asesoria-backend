4189b5c2b7dd12d27a30fbff17354a125cee4021
lasautt-ops <lasautt@gmail.com>
2026-08-28 10:35:46 +0200

    Update migration.sql

--- prisma/migrations/20260818125000_add_empresa_activo/migration.sql
+++ prisma/migrations/20260818125000_add_empresa_activo/migration.sql
@@ -1,11 +1,2 @@
 -- AlterTable
-ALTER TABLE "Empresa" ADD COLUMN     "cif" TEXT,
-ADD COLUMN     "codigoPostal" TEXT,
-ADD COLUMN     "direccion" TEXT,
-ADD COLUMN     "emailAdministrador" TEXT,
-ADD COLUMN     "nombreAdministrador" TEXT,
-ADD COLUMN     "notas" TEXT,
-ADD COLUMN     "poblacion" TEXT,
-ADD COLUMN     "provincia" TEXT,
-ADD COLUMN     "telefonoAdministrador" TEXT,
-ADD COLUMN     "web" TEXT;
+ALTER TABLE "Empresa" ADD COLUMN "activo" BOOLEAN NOT NULL DEFAULT true;
