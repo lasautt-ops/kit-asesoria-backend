c476c64a567a9da9a907539312a01268ea3ab705
lasautt-ops <lasautt@gmail.com>
2026-08-28 12:42:44 +0200

    Create migration.sql

---
+++ prisma/migrations/20260828104044_add_datos_oficina/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Oficina" ADD COLUMN     "codigoPostal" TEXT,
+ADD COLUMN     "direccion" TEXT,
+ADD COLUMN     "email" TEXT,
+ADD COLUMN     "notas" TEXT,
+ADD COLUMN     "poblacion" TEXT,
+ADD COLUMN     "provincia" TEXT,
+ADD COLUMN     "responsable" TEXT,
+ADD COLUMN     "telefono" TEXT;
