6da5f3e5b502641ca792305dc7ee42a91fa446f7
lasautt-ops <lasautt@gmail.com>
2026-08-31 13:14:59 +0200

    Create migration.sql

---
+++ prisma/migrations/20260828120513_add_cliente_datos/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Cliente" ADD COLUMN "administrador" TEXT;
+ALTER TABLE "Cliente" ADD COLUMN "cif" TEXT;
+ALTER TABLE "Cliente" ADD COLUMN "direccionFiscal" TEXT;
+ALTER TABLE "Cliente" ADD COLUMN "dniAdministrador" TEXT;
